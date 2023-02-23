b017d1ee021d048b3573bbf7724f482d43a1e3b7
Ernst Bekkering <bekkerin@users.noreply.github.com>
2023-02-23 12:13:29 -0600

    updated images for test 2

--- testing02/nsu_webpage.py
+++ testing02/nsu_webpage.py
@@ -3,6 +3,7 @@ import time
 
 # first open Chrome and go to the NSU home page
 pyautogui.press('winleft')
+pyautogui.PAUSE
 pyautogui.write('Chrome')
 pyautogui.press('enter')
 # hotkeys for Chrome can be found at https://www.howtogeek.com/114518/47-keyboard-shortcuts-that-work-in-all-web-browsers/ 
@@ -11,13 +12,14 @@ pyautogui.hotkey('ctrl','l') # shift focus to the address bar (load)
 pyautogui.write('nsuok.edu')
 pyautogui.press('enter')
 time.sleep(2)
+pyautogui.scroll(-400)
 
 # click successively on the buttons in the left column, then go back for the next button
 buttons = ['degrees_and_majors.png', 'colleges.png','academic_advising.png','bookstore.png','extended_learning.png','student_support_services.png']
 for button in buttons:
     print(f'click on {button}')
     while pyautogui.locateOnScreen(button) is None:
-        pyautogui.PAUSE = 2.5
+        pyautogui.PAUSE 
     pyautogui.click(pyautogui.center(pyautogui.locateOnScreen(button)))
     print('click back')
     pyautogui.click(pyautogui.center(pyautogui.locateOnScreen('back.png')))
