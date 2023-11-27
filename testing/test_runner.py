6b5b0ae285ce2f7c283a30e016ee5922648c9ab8
Ernst Bekkering <bekkerin@users.noreply.github.com>
2023-11-27 07:56:42 -0600

    Update test_runner.py

--- testing/test_runner.py
+++ testing/test_runner.py
@@ -9,9 +9,8 @@ import os
 executable_path = "C:\\Temp\\Testing_with_Pyautogui-main\\Testing_with_Pyautogui-main\\source\\StadiumSeating\\StadiumSeating\\bin\\Debug\\netcoreapp3.1\\StadiumSeating.exe"
 os.startfile(executable_path)
 
-#give the user a chance to kill the script
-print(' 5 second pause to let user press Ctrl-C or to click on the running file to make the window active ')
-time.sleep(5)
+#give the program time to  open
+time.sleep(1)
 
 with open('tests_with_headers.csv','r') as f: 
     # ticketsA,ticketsB,ticketsC,chargesA,chargesB,chargesC,chargesTotal,message,TestName
