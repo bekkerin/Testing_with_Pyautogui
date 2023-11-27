ae5827ee53d2d39f07ee2e9ef6c5d6805d246b5a
Ernst Bekkering <bekkerin@users.noreply.github.com>
2023-11-27 07:52:51 -0600

    include file opening
    now we no longer have to click on a running file

--- testing/test_runner.py
+++ testing/test_runner.py
@@ -4,6 +4,10 @@
 import pyautogui, time
 import pyperclip # to copy text from the revenue boxes
 from csv import DictReader 
+import os
+
+executable_path = "C:\\Temp\\Testing_with_Pyautogui-main\\Testing_with_Pyautogui-main\\source\\StadiumSeating\\StadiumSeating\\bin\\Debug\\netcoreapp3.1\\StadiumSeating.exe"
+os.startfile(executable_path)
 
 #give the user a chance to kill the script
 print(' 5 second pause to let user press Ctrl-C or to click on the running file to make the window active ')
